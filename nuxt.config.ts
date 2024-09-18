97cf302f3263f807c0fc1730689b737b8999ee21
Sakib1994 <mdnazmussakibcse@gmail.com>
2024-09-18 09:13:01 +0900

    static files generated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,13 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
   },
+  routeRules: {
+    '/': { prerender: true },
+    '/contact': { swr: 3600 },
+    '/appintment': { swr: 3600 },
+    '/faq': { prerender: true },
+    '/service': { prerender: true }
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   modules: ['@nuxtjs/color-mode', '@samk-dev/nuxt-vcalendar'],
